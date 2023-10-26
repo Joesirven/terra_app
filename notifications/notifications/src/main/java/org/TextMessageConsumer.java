974d14fb990c131fc5ff9059ddf399bf8fd4141f
Jose <jose@oldcutler.club>
2023-10-26 01:11:09 -0400

    config textmessageconsumer

--- notifications/notifications/src/main/java/org/TextMessageConsumer.java
+++ notifications/notifications/src/main/java/org/TextMessageConsumer.java
@@ -9,26 +9,46 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
-public class TextMessageConsumer {
-    private final KafkaConsumer<String, String> consumer;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
-    public NotificationConsumer(String brokers, String topic, String groupId) {
-        Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        this.consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(Collections.singletonList(topic));
+public class TextMessageConsumer {
+    private final TwilioConfig twilioConfig;
+
+    public TextMessageConsumer() {
+        this.twilioConfig = new TwilioConfig();
     }
 
-    public void consumeNotifications() {
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            records.forEach(record -> {
-                System.out.println("Received notification: " + record.value());
-                // TODO: Send text message using Twilio here
-            });
-        }
+    public void sendMesage(String toPhoneNumber, String messageBody) {
+        Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+
+        Message message = Message.creator(
+            new PhoneNumber(toPhoneNumber),
+            new PhoneNumber(twilioConfig.getFromPhoneNumber()),
+            messageBody
+        ).create();
+
+        System.out.println("Message sent: " + message.getSid());
+    }
+
+    // public TextMessageConsumer(String brokers, String topic, String groupId) {
+    //     Properties properties = new Properties();
+    //     properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+    //     properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    //     properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    //     properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    //     this.consumer = new KafkaConsumer<>(properties);
+    //     consumer.subscribe(Collections.singletonList(topic));
+    // }
+
+    // public void consumeNotifications() {
+    //     while (true) {
+    //         ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+    //         records.forEach(record -> {
+    //             System.out.println("Received notification: " + record.value());
+    //             // TODO: Send text message using Twilio here
+    //         });
+    //     }
     }
 }
--- notifications/notifications/src/main/java/org/TwilioConfig.java
+++ notifications/notifications/src/main/java/org/TwilioConfig.java
@@ -1,4 +1,26 @@
 package org;
 
 public class TwilioConfig {
+    private String accountSid;
+    private String authToken;
+    private String fromPhoneNumber;
+
+    public TwilioConfig() {
+        this.accountSid = System.getenv("TWILIO_ACCOUNT_SID");
+        this.authToken = System.getenv("TWILIO_AUTH_TOKEN");
+        this.fromPhoneNumber = System.getenv("TWILIO_FROM_PHONE_NUMBER");
+    }
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public String getFromPhoneNumber() {
+        return fromPhoneNumber;
+    }
+
 }
