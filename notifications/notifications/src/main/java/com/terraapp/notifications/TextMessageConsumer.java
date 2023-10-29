2821bd85c9b606aa50e84ba77e41cf0e2df2d684
Jose <jose@oldcutler.club>
2023-10-29 17:07:40 -0400

    catch exception to kafka consumer poller

--- notifications/notifications/src/main/java/com/terraapp/notifications/TextMessageConsumer.java
+++ notifications/notifications/src/main/java/com/terraapp/notifications/TextMessageConsumer.java
@@ -17,33 +17,38 @@ public class TextMessageConsumer {
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "your_group_id");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "terra-notification-sms");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // Create Kafka consumer
         Consumer<String, String> consumer = new KafkaConsumer<>(properties);
 
         // Subscribe to topic
-        consumer.subscribe(Collections.singleton("your_topic_name"));
+        consumer.subscribe(Collections.singleton("terra-notification-sms"));
 
         // Load Twilio configuration
         TwilioConfig twilioConfig = new TwilioConfig();
         TwilioSender twilioSender = new TwilioSender(twilioConfig);
 
-        // Poll for new data
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        // Poll for new data with error handling
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-            records.forEach(record -> {
-                System.out.printf("Received record with key %s and value %s%n", record.key(), record.value());
+                records.forEach(record -> {
+                    System.out.printf("Received record with key %s and value %s%n", record.key(), record.value());
 
-                // TODO: Extract phone number and message from record.value() and validate them
-                String phoneNumber = "+1234567890"; // Placeholder value
-                String message = record.value(); // Using the record value directly as the message
+                    // TODO: Extract phone number and message from record.value() and validate them
+                    String phoneNumber = "+1234567890"; // Placeholder value
+                    String message = record.value(); // Using the record value directly as the message
 
-                // Send SMS
-                twilioSender.sendMessage(phoneNumber, message);
-            });
+                    // Send SMS
+                    twilioSender.sendMessage(phoneNumber, message);
+                });
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            consumer.close();
         }
     }
 }
