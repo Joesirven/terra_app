2204f60180094a71a2c6f2f3e6466997f17f3048
Jose <jose@sirven.xyz>
2024-03-24 18:41:43 -0400

    fixed async bug with home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,27 +1,34 @@
-"use server"
-
+"use client"
+import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-
 export default function Home() {
-  async function retrieveWorkOSURL() {
-    const res = await fetch('{process.env.NEXT_PUBLIC_API_URL}/auth/login')
+  const [url, setUrl] = useState('');
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch login url')
+  useEffect(() => {
+    async function retrieveWorkOSURL() {
+      const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/login`;
+      console.log('URL:', url);
+
+      const res = await fetch(url);
+
+      if (!res.ok) {
+        throw new Error('Failed to fetch login url');
+      }
+
+      const data = await res.json();
+      setUrl(data.url);
     }
 
-    return res.json()
-  };
-
+    retrieveWorkOSURL();
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
       <Button asChild>
-        <Link href={retrieveWorkOSURL}>Login</Link>
+        <Link href={url}>Login</Link>
       </Button>
     </main>
-  )
+  );
 }
